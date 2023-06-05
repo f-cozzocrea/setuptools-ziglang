be343d6719f0351917f05151a320a6fdc9b269fa
february cozzocrea <february.cozzocrea@gmail.com>
2023-06-05 12:12:30 -0700

    noxfile updates

--- noxfile.py
+++ noxfile.py
@@ -3,21 +3,19 @@ import nox
 python_versions = ["3.7", "3.8", "3.9", "3.10", "3.11",] ## "pypy3.8", "pypy3.9"]
 
 @nox.session(python=python_versions)
-def test_c(session):
+def test(session):
     session.install(".")
     session.install("build")
 
-    with session.chdir("testing/c_sum"):
-       session.run(session.python, '-m', 'build')
-       session.run()
+    with session.chdir("testing/c"):
+        session.run('rm', '-rf', 'build/', 'dist/',)
+        session.run(session.python, '-m', 'build')
+        session.install('dist/*.whl')
+
+    with session.chdir("testing/zig"):
+        session.run('rm', '-rf', 'build/', 'dist/',)
+        session.run(session.python, '-m', 'build')
+        session.install('dist/*.whl')
 
 
-@nox.session(python=python_versions)
-def test_zig(session):
-    session.install("build")
-
-    with session.chdir("testing/zig_sum"):
-
-
-    session.install("pytest")
 
