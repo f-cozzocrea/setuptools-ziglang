c68571eb89255acdb3dc7ccfdb2917ca6c4a41cf
february cozzocrea <february.cozzocrea@gmail.com>
2023-06-04 17:09:23 -0700

    metadata added to setup.cfg

--- setuptools_ziglang.py
+++ setuptools_ziglang.py
@@ -1,6 +1,6 @@
 from setuptools.command.build_ext import build_ext
-from setuptools.errors import CompileError
-from setuptools._distutils.ccompiler import CCompiler
+
+import sys
 
 class build_zig_ext(build_ext):
     def build_extensions(self):
@@ -10,13 +10,10 @@ class build_zig_ext(build_ext):
         self.compiler.set_executable("linker_so", ["zig", "cc", "-shared"])
         self.compiler.set_executable("linker_exe", ["zig", "cc"])
         self.compiler.set_executable("archiver", ["zig", "ar", "-cr"])
-        self.compiler.set_executable("ranlib", None)
+        
+        if sys.platform[:6] == "darwin":
+            self.compiler.set_executable("ranlib", ["zig", "ranlib"])
+    
         build_ext.build_extensions(self)
 
-
-class ZigCompiler(CCompiler):
-    pass
-
-class ZigCompileError(CompileError):
-    pass
 
