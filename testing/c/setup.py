e684d96fe644dc900f9ddeef957a134b474a76c4
february cozzocrea <february.cozzocrea@gmail.com>
2023-06-05 20:51:18 -0700

    pyproject.toml added to c-ext in testing

--- testing/c/setup.py
+++ testing/c/setup.py
@@ -1,9 +1,6 @@
 from setuptools import Extension, setup
 
 setup(
-    name="c-ext",
-    version='0.0.0',
     ext_modules=[Extension("c_ext", ["c_ext.c"])],
-    setup_requires=['setuptools-ziglang'],
 )
 
