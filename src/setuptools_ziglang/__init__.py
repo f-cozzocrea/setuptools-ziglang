eadeaf61573a15f53152ffd44febfa4e0a55da21
february cozzocrea <february.cozzocrea@gmail.com>
2023-06-09 14:43:16 -0700

    __init__.py updates

--- src/setuptools_ziglang/__init__.py
+++ src/setuptools_ziglang/__init__.py
@@ -2,29 +2,40 @@ from setuptools_ziglang import zigcompiler
 from setuptools_ziglang.zigextension import ZigExtension as Extension
 
 import os
-import distutils
+import sys
 from setuptools import Distribution
-from setuptools._distutils import ccompiler
 from setuptools.command.build_ext import build_ext
-from wheel.bdist_wheel import bdist_wheel
 
 __all__ = ['Extension']
 
 
-# Add the Zig language and compiler to the setuptools list of compilers
-ccompiler.compiler_class['zig'] = ("zigcompiler", "ZigCompiler", "The Zig language compiler")
-ccompiler.CCompiler.language_map[".zig"] = "zig"
-ccompiler.CCompiler.language_order.insert(0, "zig")
-distutils.zigcompiler = zigcompiler
+# Add the Zig language and compiler to the list of compilers
+def patch_distutils_ccompiler(module):
+    module.ccompiler.compiler_class['zig'] = ("zigcompiler", "ZigCompiler", "The Zig language compiler")
+    module.ccompiler.CCompiler.language_map[".zig"] = "zig"
+    module.ccompiler.CCompiler.language_order.insert(0, "zig")
+    module.zigcompiler = zigcompiler
+    # Users can set this if they want Zig as the default compiler. May be useful if the compiler mutates
+    # because of other extensions
+    if os.getenv("SETUPTOOLS_FORCE_ZIG", None):
+        module.ccompiler._default_compilers = (
+            ('cygwin.*', 'zig'),
+            ('posix', 'zig'),
+            ('nt', 'zig'),
+        )
 
-# Users can set this if they want Zig as the default compiler. May be useful if the compiler mutates
-# because of other extensions
-if os.getenv("SETUPTOOLS_FORCE_ZIG", None):
-    ccompiler._default_compilers = (
-        ('cygwin.*', 'zig'),
-        ('posix', 'zig'),
-        ('nt', 'zig'),
-    )
+
+
+import setuptools._distutils as dist_module
+patch_distutils_ccompiler(dist_module)
+
+# Patch distutils stdlib unless we're in a Python version where they've been removed (>= 3.12)
+if sys.version_info[1] >= 12:
+    try:
+        import distutils
+        patch_distutils_ccompiler(distutils)
+    except (ModuleNotFoundError, ImportError) as ModuleImportError:
+        pass
 
 
 # dist finalize options entry point
