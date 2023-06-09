7a71c21efaefea5a5fc9b6095473e3c72082aa32
february cozzocrea <february.cozzocrea@gmail.com>
2023-06-08 21:05:21 -0700

    ZigCompiler updates

--- src/setuptools_ziglang/zigcompiler.py
+++ src/setuptools_ziglang/zigcompiler.py
@@ -8,7 +8,7 @@ from importlib.util import find_spec
 class ZigCompiler(UnixCCompiler):
     compiler_type = 'zig'
 
-    # Prefer using the PyPI zig binary if it exists
+    # Prefer using the PyPI zig binary, else fall back to the system zig
     if find_spec("ziglang"):
         zig_bin = [sys.executable, '-m', 'ziglang',]
     else:
@@ -29,6 +29,22 @@ class ZigCompiler(UnixCCompiler):
 
     if sys.platform[:6] == "darwin":
         executables['ranlib'] = [*zig_bin, "ranlib"]
+
+    # Needed for the filename generation methods provided by the base
+    # class, CCompiler.  NB. whoever instantiates/uses a particular
+    # UnixCCompiler instance should set 'shared_lib_ext' -- we set a
+    # reasonable common default here, but it's not necessarily used on all
+    # Unices!
+    src_extensions = [".zig", ".c", ".C", ".cc", ".cxx", ".cpp", ".m"]
+    obj_extension = ".o"
+    static_lib_extension = ".a"
+    shared_lib_extension = ".so"
+    dylib_lib_extension = ".dylib"
+    xcode_stub_lib_extension = ".tbd"
+    static_lib_format = shared_lib_format = dylib_lib_format = "lib%s%s"
+    xcode_stub_lib_format = dylib_lib_format
+    if sys.platform == "cygwin":
+        exe_extension = ".exe"
 
 
 
