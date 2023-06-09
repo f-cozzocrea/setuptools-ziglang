888567588a3752eaca16febd8c9fdd69fc96efcb
february cozzocrea <february.cozzocrea@gmail.com>
2023-06-09 13:23:27 -0700

    nox command order changed

--- noxfile.py
+++ noxfile.py
@@ -4,8 +4,8 @@ python_versions = ["2.7", "3.7", "3.8", "3.9", "3.10", "3.11",] ## "pypy3.8", "p
 
 @nox.session(python=python_versions)
 def test(session):
-    session.install(".")
     session.install("build")
+    session.install(".")
 
     with session.chdir("testing/c"):
         session.run('rm', '-rf', 'build/', 'dist/',)
