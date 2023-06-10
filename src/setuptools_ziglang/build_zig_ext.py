388466725e85f709e7eb0bd13e963c2e13a9bd40
february cozzocrea <february.cozzocrea@gmail.com>
2023-06-09 17:12:31 -0700

    BuildZigExt updates

--- src/setuptools_ziglang/build_zig_ext.py
+++ src/setuptools_ziglang/build_zig_ext.py
@@ -2,8 +2,67 @@ from setuptools_ziglang.zigcompiler import ZigCompiler
 #from setuptools_ziglang.zigextension import ZigExtension
 
 from setuptools.command.build_ext import build_ext
+from setuptools._distutils.sysconfig import customize_compiler
 
 class BuildZigExt(build_ext):
-    def build_extension(self, ext):
-        self.compiler = ZigCompiler() 
-        build_ext.build_extension(self, ext)
+    #def build_extension(self, ext):
+    #    self.compiler = ZigCompiler()
+    #    breakpoint()
+    #    build_ext.build_extension(self, ext)
+
+    def run(self):
+        from setuptools_ziglang.zigcompiler import ZigCompiler
+
+        # 'self.extensions', as supplied by setup.py, is a list of
+        # Extension instances.  See the documentation for Extension (in
+        # distutils.extension) for details.
+        #
+        # For backwards compatibility with Distutils 0.8.2 and earlier, we
+        # also allow the 'extensions' list to be a list of tuples:
+        #    (ext_name, build_info)
+        # where build_info is a dictionary containing everything that
+        # Extension instances do except the name, with a few things being
+        # differently named.  We convert these 2-tuples to Extension
+        # instances as needed.
+        
+        breakpoint()
+        if not self.extensions:
+            return
+        
+        # If we were asked to build any C/C++ libraries, make sure that the
+        # directory where we put them is in the library search path for
+        # linking extensions.
+        if self.distribution.has_c_libraries():
+            build_clib = self.get_finalized_command('build_clib')
+            self.libraries.extend(build_clib.get_library_names() or [])
+            self.library_dirs.append(build_clib.build_clib)
+
+        self.compiler = ZigCompiler()
+        customize_compiler(self.compiler)
+
+        # And make sure that any compile/link-related options (which might
+        # come from the command-line or from the setup script) are set in
+        # that CCompiler object -- that way, they automatically apply to
+        # all compiling and linking done here.
+        if self.include_dirs is not None:
+            self.compiler.set_include_dirs(self.include_dirs)
+        if self.define is not None:
+            # 'define' option is a list of (name,value) tuples
+            for name, value in self.define:
+                self.compiler.define_macro(name, value)
+        if self.undef is not None:
+            for macro in self.undef:
+                self.compiler.undefine_macro(macro)
+        if self.libraries is not None:
+            self.compiler.set_libraries(self.libraries)
+        if self.library_dirs is not None:
+            self.compiler.set_library_dirs(self.library_dirs)
+        if self.rpath is not None:
+            self.compiler.set_runtime_library_dirs(self.rpath)
+        if self.link_objects is not None:
+            self.compiler.set_link_objects(self.link_objects)
+
+        # Now actually compile and link everything.
+        self.build_extensions()
+
+
